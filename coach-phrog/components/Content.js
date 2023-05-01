9ec9eac90f200cd119b50679270263faf165c232
InTheRiches <72281809+InTheRiches@users.noreply.github.com>
2023-05-01 18:39:49 +0000

    Fixed formatting

--- coach-phrog/components/Content.js
+++ coach-phrog/components/Content.js
@@ -4,8 +4,8 @@ export default function Content({title, content}) {
 
     return (
         <div className="ml-2 w-3/4 px-4">
-            <span className="text-3xl font-bold mb-8">{title}</span>
-            <div className="flex flex-col">
+            <span className="text-3xl font-bold">{title}</span>
+            <div className="flex flex-col mt-8">
                 {spans.map((substring, index) => (
                     <span className="text-md mb-2" key={index}>{substring}</span>
                 ))}
--- coach-phrog/pages/index.js
+++ coach-phrog/pages/index.js
@@ -19,8 +19,8 @@ export default function Layout({ children }) {
         <div className="flex-1 flex-col container mx-auto my-8 flex">
             {/* Page Header */}
             <span className="ml-6 mb-14 text-6xl font-bold">Muscle Basics</span>
+            <span>Muscles are a type of tissue in our bodies that allow us to move and carry out physical activities. Think of muscles like rubber bands or ropes that are attached to our bones and can contract, or shorten, to pull the bones closer together, allowing us to move our bodies.-.-For example, when you bend your arm, your bicep muscle contracts, pulling your forearm towards your shoulder. When you straighten your arm, your tricep muscle contracts, pulling your forearm away from your shoulder.</span>
             {/* First Topic */}
-            <Content title="Muscle Definition" content="Muscles are a type of tissue in our bodies that allow us to move and carry out physical activities. Think of muscles like rubber bands or ropes that are attached to our bones and can contract, or shorten, to pull the bones closer together, allowing us to move our bodies.-.-For example, when you bend your arm, your bicep muscle contracts, pulling your forearm towards your shoulder. When you straighten your arm, your tricep muscle contracts, pulling your forearm away from your shoulder."></Content>
             <Content title="Muscle Activation" content="Testing"></Content>
         </div>
 
