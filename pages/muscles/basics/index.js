bc9c2f3410dd6df978bcad38fe13f44856a951d7
InTheRiches <72281809+InTheRiches@users.noreply.github.com>
2023-05-11 19:06:38 +0000

    Added content

--- pages/muscles/basics/index.js
+++ pages/muscles/basics/index.js
@@ -29,26 +29,6 @@ export default function MuscleBasics({}) {
                 title={"Muscle Growth"}
                 content={"When you use your muscles to perform physical activities, such as lifting weights, your muscles can get bigger and stronger. This happens because your muscles are made up of tiny fibers, and when you challenge these fibers by lifting heavy weights, it causes them to **tear** and break down.-.-This might sound bad, but it's actually a good thing! When these tiny fibers are damaged, it triggers a response from your body to **repair** and rebuild them **stronger** and thicker than before. This process is called muscle [/muscles/training/hypertrophy,hypertrophy] (hy·per·tro·phy), and it's how muscles grow.-.-To support muscle growth, you need to give your body the right fuel and rest. Eating a balanced diet that includes plenty of protein is important because protein helps to repair and build new muscle fibers. Getting enough sleep and rest is also crucial because it allows your body to **recover** and **rebuild** the muscles that were damaged during exercise."}
             ></Content>
-            <div className="ml-6">
-                <div className="flex rounded-md border-1 border-yellow-200 bg-yellow-50 dark:bg-black dark:bg-opacity-20 p-4 text-lg text-yellow-500 dark:text-yellow-200">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="mr-3 1.2 h-5 w-5 flex-shrink-0">
-                        <path fillRule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
-                    </svg>
-                    <div className={"w-full"}>
-                        <h4 className="font-bold">Make sure to strengthen the rotator cuff as you increase load on shoulder bearing movements, as permanent damage can occur.</h4>
-                        <div className="mt-1">
-                            <ul className="list-inside list-disc lg:grid w-full">
-                                <li>Reduced performance and endurance</li>
-                                <li>Muscle cramps and spasms</li>
-                                <li>Increased risk of heat exhaustion and heatstroke</li>
-                                <li>Impaired cognitive function and concentration</li>
-                                <li>Reduced metabolic rate and digestion</li>
-                                <li>Kidney problems</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <Content 
                 id="muscle-heads"
                 title={"Muscle Heads"}
@@ -57,7 +37,7 @@ export default function MuscleBasics({}) {
             <div className="ml-6">
                 <div className="flex rounded-md border-1 border-sky-500 p-4 dark:bg-black dark:bg-opacity-20 text-lg text-sky-600 bg-blue-50">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                            className="mr-3 1.2 h-5 w-5 flex-shrink-0">
+                            className="mr-3 mt-1.2 h-5 w-5 flex-shrink-0">
                         <path fillRule="evenodd"
                                 d="M19 10.5a8.5 8.5 0 11-17 0 8.5 8.5 0 0117 0zM8.25 9.75A.75.75 0 019 9h.253a1.75 1.75 0 011.709 2.13l-.46 2.066a.25.25 0 00.245.304H11a.75.75 0 010 1.5h-.253a1.75 1.75 0 01-1.709-2.13l.46-2.066a.25.25 0 00-.245-.304H9a.75.75 0 01-.75-.75zM10 7a1 1 0 100-2 1 1 0 000 2z"
                                 clipRule="evenodd"/>
