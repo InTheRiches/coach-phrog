3aa45bbf77086b47a73fc342ece7055941b48228
InTheRiches <72281809+InTheRiches@users.noreply.github.com>
2023-06-02 22:45:03 +0000

    adjusted landing page

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Layout() {
             <header className={"flex flex-col items-center w-full h-screen"}>
                 <Navigation dark={isDarkMode} setDark={toggleDarkMode}></Navigation>
                 <div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32 pl-3 pr-3">
-                    <h1 className={"text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white"}>Transform your physique with expert guidance without ever paying a dollar.</h1>
+                    <h1 className={"text-slate-900 font-extrabold text-3xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white"}>Transform your physique with expert guidance without ever paying a dollar.</h1>
                     <p className="mt-6 text-lg lg:text-2xl text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">A science based approach to <a className={"text-cyan-accent"}>building muscle</a>, <a className={"text-cyan-accent"}>losing fat</a>, and getting <a className={"text-cyan-accent"}>stronger</a>, formatted to be understood and accessible by all.</p>
 
                     <div className="mt-6 sm:mt-10 flex justify-center space-x-6 text-sm"><a
