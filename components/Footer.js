2b071048307ec8c8c248136b6fff3e3b791d57c1
John Doe <intherichesftk@gmail.com>
2023-06-14 22:28:02 -0400

    Fixed footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer({ darkBG = "neutral-900" }) {
     return <footer className={`bg-white dark:bg-${darkBG} text-neutral-900 dark:text-slate-50 py-4`}>
         <div className="flex items-center justify-center text-center">
-            <p>&copy; 2023 Surge Strength. All rights reserved.</p>
+            <p>&copy; 2023 Wavelength. All rights reserved.</p>
         </div>
     </footer>
 }
